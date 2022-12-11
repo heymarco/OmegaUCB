Fixed bug in eta computation

facebook_ad_data_util.py
@@ -35,9 +35,11 @@ def sort_df(df):
 
 def get_setting(df):
     df = sort_df(df)
-    mean_rewards = list(df["revenue_per_1000_impressions"])
-    mean_costs = list(df["cost_per_1000_impressions"])
-    return np.array(mean_rewards), np.array(mean_costs)
+    mean_rewards = np.array(df["revenue_per_1000_impressions"])
+    mean_costs = np.array(df["cost_per_1000_impressions"])
+    mean_costs = mean_costs[mean_costs > 0]
+    mean_rewards = mean_rewards[mean_costs > 0]
+    return mean_rewards, mean_costs
 
 
 def add_noise(setting, random_state: int):
@@ -69,14 +71,14 @@ def get_facebook_ad_data_settings(random_state: int):
 
 def get_facebook_ad_stats():
     data = prepare_facebook_data()
-    cols = ["Age", "Gender", "c_min", "K"]
+    cols = ["campaign_id", "age", "gender", "c_min", "K"]
     rows = []
     for (cid, age, gender), gdf in data.groupby(["campaign_id", "age", "gender"]):
         setting = get_setting(gdf)
         k = len(setting[0])
         if k >= 2:
             rows.append([
-                age, gender, min(setting[-1]), len(setting[0])
+                cid, age, gender, min(setting[-1]), len(setting[0])
             ])
     df = pd.DataFrame(rows, columns=cols)
-    print(df.sort_values(by=["Gender", "Age"]).set_index(["Gender", "Age"]).to_latex(escape=False, multirow=True))
+    print(df.sort_values(by=["age", "gender"]).set_index(["age", "gender"]).to_latex(escape=False, multirow=True))

util.py
@@ -76,11 +76,12 @@ def subsample_csv(csv_path: str, every_nth: int = 1):
 def create_palette(df: pd.DataFrame):
     df.sort_values(by=[APPROACH_ORDER, RHO])
     ts = [BTS]
-    wucb = [OMEGA_UCB_, ETA_UCB_]
+    wucb = [OMEGA_UCB_]
+    eta_ucb = [ETA_UCB_]
     ucb = [MUCB, CUCB, IUCB, BUDGET_UCB]
     ucb_sc = [UCB_SC_PLUS]
-    id_list = [ts, ucb_sc, ucb, wucb]
-    color_palettes = ["Wistia", "Reds", "Greens", "Blues"]
+    id_list = [ts, ucb_sc, ucb, wucb, eta_ucb]
+    color_palettes = ["Wistia", "Reds", "Greens", "Blues", "Purples"]
     final_palette = []
     for c, ids in zip(color_palettes, id_list):
         mask = df[APPROACH].apply(lambda x: np.any([id in x for id in ids])).astype(bool)
@@ -100,9 +101,9 @@ def cm2inch(*tupl):
 
 
 def extract_rho(s: str):
-    if "rho=" not in s:
+    if "rho" not in s:
         return np.nan
-    relevant_part = s.split("=")[-1][:-1]
+    relevant_part = s.split("=")[-1].split("$")[0]
     if "/" in relevant_part:
         numerator, denominator = relevant_part.split("/")
         numerator = float(numerator)