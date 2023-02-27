3a8ca1b56b059a69ef7e6c163c64a67d101dd174
Marco Heyden <marco.heyden@kit.edu>
2023-02-27 13:53:15 +0100

    Facebook experiment

--- facebook_ad_data_util.py
+++ facebook_ad_data_util.py
@@ -51,7 +51,7 @@ def add_noise(setting, rng):
 def sort_setting(setting):
     rew = setting[0]
     cost = setting[1]
-    efficiency_inv = cost / rew if rew > 0 else np.infty
+    efficiency_inv = [c / r if r > 0 else np.infty for c, r in zip(cost, rew)]
     argsort = np.argsort(efficiency_inv)
     return rew[argsort], cost[argsort]
 
